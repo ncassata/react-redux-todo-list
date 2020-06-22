a1060fe026c5947ef7ba6b6bc7c0d7548d89c61b
cassatanathan <cassatanathan@gmail.com>
2020-06-22 15:56:50 -0700

    Continue webpack setup

--- src/index.tsx
+++ src/index.tsx
@@ -1,6 +1,11 @@
 import * as React from "react";
 import * as ReactDOM from "react-dom";
 
+import { App } from "./components/App";
+import "./styles/style.scss";
+
 const ROOT = document.querySelector(".container");
 
-ReactDOM.render(<h1>Hello</h1>, ROOT);
+const app = App;
+
+ReactDOM.render(<App name="Kofi"/>, ROOT);
--- webpack.config.js
+++ webpack.config.js
@@ -1,4 +1,7 @@
 const path = require('path');
+const HtmlWebpackPlugin = require('html-webpack-plugin');
+const MiniCssExtractPlugin = require('mini-css-extract-plugin');
+const OptimizeCSSAssetsPlugin = require('optimize-css-assets-webpack-plugin')
 
 module.exports ={
   entry: './src/index.tsx',
@@ -6,13 +9,43 @@ module.exports ={
     path: path.resolve(__dirname, "build"),
     filename: "bundle.js"
   },
+  optimization: {
+    minimizer: [new OptimizeCSSAssetsPlugin({})],
+  },
   module: {
     rules: [
       {
         test: /\.tsx?$/,
         loader: "awesome-typescript-loader"
+      },
+      {
+        enforce: "pre",
+        test: /\.js$/,
+        loader: "source-map-loader"
+      },
+      {
+        test: /\.scss$/,
+        use: [
+          MiniCssExtractPlugin.loader,
+          {
+            loader: "css-loader",
+            options: {
+              sourceMap: true,
+            }
+          },
+          "sass-loader"
+        ]
       }
     ]
   },
-  plugins: []
+  plugins: [
+    new HtmlWebpackPlugin({
+      template:"./index.html"
+    }),
+    new MiniCssExtractPlugin("style.css")
+  ],
+  devtool: "source-map",
+  resolve:{
+    extensions: [".js", ".ts", ".tsx"]
+  }
 }
